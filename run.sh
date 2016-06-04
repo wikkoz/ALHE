2f2cc505c4e8f49f684e1e56035fc114dd7615f6
Konrad Ryczek <konrad.ryczek@sollers.eu>
2016-06-04 18:24:43 +0200

    script qfix

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-R CMD BATCH --slave ./Astar.R &
+R CMD BATCH --slave ./alhe1.R
+mv 1\ test.png 1test_$(date +%F-%T).png
+mv 2\ test.png 2test_$(date +%F-%T).png
+mv 3\ test.png 3test_$(date +%F-%T).png
+mv 4\ test.png 4test_$(date +%F-%T).png
 exit 0
